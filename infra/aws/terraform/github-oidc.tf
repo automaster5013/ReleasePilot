f8adf0c6d12a66f9901fc98aa92778cc915d3842
myeonghwi <automaster5013@gmail.com>
2026-09-14 19:28:01 +0900

    fix: trust immutable GitHub OIDC subject

--- infra/aws/terraform/github-oidc.tf
+++ infra/aws/terraform/github-oidc.tf
@@ -23,10 +23,7 @@ data "aws_iam_policy_document" "github_release_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values = [
-        "repo:automaster5013/ReleasePilot:ref:refs/tags/v*",
-        "repo:automaster5013/releasepilot:ref:refs/tags/v*",
-      ]
+      values   = ["repo:automaster5013@247691206/ReleasePilot@1369198899:ref:refs/tags/v*"]
     }
   }
 }
